28aec06518e7c9206a2db03adb6b3bbb0e2c56c0
kerkurt <kubilayerkurt@icloud.com>
2025-12-18 16:24:12 +0300

    Created the player, the mighty triangle that waits for moving.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     while True:
@@ -15,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0  # Limit to 60 FPS
         # print(f"Delta time for this frame: {dt:.4f} seconds")
